0b8ab5b13d01a2b815dff89a2275920c8e9bcd6b
Michael Dunsterville <michael@klientboost.com>
2020-09-24 12:54:46 -0700

    Initial commit

---
+++ Cost Deviation.js
@@ -0,0 +1,74 @@
+var COST_PRECENT_DIFFERENCE = 0.10; // Precent difference 0.10 = 10%
+var Email_ids = "your@email.it"//Insert your Email id here i.e "abc@gmail.com,abc@xyz.com"
+function main() {
+  send_account_notification();
+}
+function send_account_notification() {
+  Logger.log("Script running started..");
+  var startDate = getPastDateAdwords(1);
+  var endDate = getPastDateAdwords(2);
+  var resStart = getData(startDate);
+  var resEnd = getData(endDate);
+  var accountReportStart = AdWordsApp.report(resStart);
+  var accountReportEnd = AdWordsApp.report(resEnd);
+  var rowsStart = accountReportStart.rows();
+  var rowsEnd = accountReportEnd.rows();
+  while (rowsStart.hasNext()) {
+    var row = rowsStart.next();
+    var CostStart = row['Cost']
+    CostStart = parseFloat(CostStart)
+  }
+  while (rowsEnd.hasNext()) {
+    var row = rowsEnd.next();
+    var CostEnd = row['Cost']
+    CostEnd = parseFloat(CostEnd)
+    var account_name = row['AccountDescriptiveName']
+  }  
+  if (Math.abs((CostStart - CostEnd) / ((CostStart + CostEnd) / 2)) > COST_PRECENT_DIFFERENCE) {
+    Logger.log(" Cost change is below threshold.. send email")
+    sendMail(account_name, (CostStart - CostEnd) / ((CostStart + CostEnd) / 2)*100)
+  }
+  else {
+    Logger.log(" Cost change is above threshold.. do not send email..")
+  }
+  //=====================send email notification=================================
+  function sendMail(account_name, PrecentDifference) {
+    var html = [];
+    html.push(
+      "<html>",
+      "<body>",
+      "<table width=100% cellpadding=0 border=0 cellspacing=0>",
+      "<tr bgcolor='#3c78d8′>",
+      "<td width=500>",
+      "<div style='font: normal 18pt verdana, sans-serif; padding: 3px 10px; color: white'>Cost Precent Difference Alert</div>",
+      "</td>",
+      "<td align=right>",
+      "<div style='font: normal 18pt verdana, sans-serif; padding: 3px 10px; color: white'>" + account_name + "</h1>",
+      "</td>",
+      "</tr>",
+      "</table>",
+      "<table width=100% cellpadding=0 border=1 cellspacing=0>",
+      "<tr bgcolor='#ddd'>",
+      "<td style='font: 12pt verdana, sans-serif; padding: 5px 0px 5px 5px; background-color: #ddd; text-align: center'>Cost has changed by: " + PrecentDifference + "%</td>",
+      "</tr>",
+      "</table>",
+      "</body>",
+      "</html>");
+    MailApp.sendEmail(Email_ids, 'KB Cost Precent Difference Threshold Script', "", { htmlBody: html.join("\n") });
+  }
+  //*****************************************************************************
+  function getData(date) {
+    var query = "SELECT ExternalCustomerId, AccountDescriptiveName, " +
+      "Cost " +
+      "FROM ACCOUNT_PERFORMANCE_REPORT " +
+      "DURING " + date + ',' + date;
+    return query;
+  }
+  //################################################################################
+  function getPastDateAdwords(numDays) {
+    var timeZone = AdWordsApp.currentAccount().getTimeZone()
+    var today = new Date();
+    today.setDate(today.getDate() - numDays);
+    return Utilities.formatDate(today, timeZone, 'yyyyMMdd');
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# google-ads-scripts
+# Dunsterville's Google Ads Scripts
+
+These scripts are to be used for Google Ads and will send alerts to help you keep track of performance in the account
+
+## Current Scripts
+  - Precent Deviation of CPA
+  - Precent Deviation of Cost
+
+##### Precent Deviation of Cost
+[Link to script](https://github.com/dunsterville/google-ads-scripts/cost_deviation.js)
+
+It is defaulted to 10% Day over Day, you can change that on line 1 like so
+```js
+var COST_PRECENT_DIFFERENCE = 0.05; // 5%
+var COST_PRECENT_DIFFERENCE = 0.15; // 15%
+var COST_PRECENT_DIFFERENCE = 0.20; // 20%
+```
+
+At Line 2 you will enter the email(s) that you want the alert to be sent to
+```js
+var Email_ids = "your@email.it, example@domain.com"
+```
+
+You can also change the Subject and content of the alert. The body of the emails is from lines 38-56 and the subject is on line 57 defaulted to "KB Cost Precent Difference Threshold Script"
+
+##### Precent Deviation of CPA
+[Link to script](https://github.com/dunsterville/google-ads-scripts/cpa_deviation.js)
+
+Very similiar to Cost Deviation. It is defaulted to 10% Day over Day, you can change that on line 1 like so
+```js
+var CPA_PRECENT_DIFFERENCE = 0.05; // 5%
+var CPA_PRECENT_DIFFERENCE = 0.15; // 15%
+var CPA_PRECENT_DIFFERENCE = 0.20; // 20%
+```
+
+At Line 2 you will enter the email(s) that you want the alert to be sent to
+```js
+var Email_ids = "your@email.it, example@domain.com"
+```
+
+You can also change the Subject and content of the alert. The body of the emails is from lines 38-56 and the subject is on line 57 defaulted to "KB CPA Precent Difference Threshold Script"
+
+### Future Scripts
+  - Precent Deviation of ROAS WoW
+  - Precent Deviation of Revenue WoW
+
+
+If you have any questions about the use of these scripts please email me [michael@klientboost.com](mailto:michael@klientboost.com?subject=Google%20Ads%20Scripts) 
+  
